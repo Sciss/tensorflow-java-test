c2e6643ab5b6bc7d99e99a40fd036dc337162827
Hanns Holger Rutz <contact@sciss.de>
2021-10-04 16:56:37 +0200

    ... <>

--- src/main/scala/de/sciss/tf/CnnGanMnist.scala
+++ src/main/scala/de/sciss/tf/CnnGanMnist.scala
@@ -16,31 +16,31 @@ object CnnGanMnist:
 
   end main
 
-//  def makeGeneratorModel(): Model[TFloat32] =
-//    val model = Sequential.of(classOf[TFloat32],
-//      Layers.dense(7*7*256, /*Dense.Options.builder()*/ use_bias=false, input_shape=(100,???)),
-//      Layers.batchNormalization(),
-//      Layers.leakyReLU(),
-//    )
-//    model.add(Layers.reshape((7, 7, 256)))
-//    assert (model.output_shape == (None, 7, 7, 256))  // Note: None is the batch size
-//
-//    model.add(Layers.conv2DTranspose(128, (5, 5), strides=(1, 1), padding="same", use_bias=false))
-//    assert (model.output_shape == (None, 7, 7, 128))
-//    model.add(Layers.batchNormalization())
-//    model.add(Layers.leakyReLU())
-//
-//    model.add(Layers.conv2DTranspose(64, (5, 5), strides=(2, 2), padding="same", use_bias=false))
-//    assert (model.output_shape == (None, 14, 14, 64))
-//    model.add(Layers.batchNormalization())
-//    model.add(Layers.leakyReLU())
-//
-//    model.add(Layers.conv2DTranspose(1, (5, 5), strides=(2, 2), padding="same", use_bias=false, activation="tanh"))
-//
-//    assert (model.output_shape == (None, 28, 28, 1))
-//
-//    model
-//  end makeGeneratorModel
+  def makeGeneratorModel(): Model[TFloat32] =
+    val model = Sequential.of(classOf[TFloat32],
+      Layers.input(100),
+      Layers.dense(7*7*256), // , /*Dense.Options.builder()*/ use_bias=false),
+      ???, // Layers.batchNormalization(),
+      Layers.leakyReLU(),
+
+      ???, // Layers.reshape((7, 7, 256)),
+      // assert (model.output_shape == (None, 7, 7, 256))  // Note: None is the batch size
+
+      ???, // Layers.conv2DTranspose(128, (5, 5), strides=(1, 1), padding="same", use_bias=false),
+      // assert (model.output_shape == (None, 7, 7, 128))
+      ???, // Layers.batchNormalization(),
+      Layers.leakyReLU(),
+
+      ???, // Layers.conv2DTranspose(64, (5, 5), strides=(2, 2), padding="same", use_bias=false),
+      // assert (model.output_shape == (None, 14, 14, 64))
+      ???, // Layers.batchNormalization(),
+      Layers.leakyReLU(),
+
+      ???, // Layers.conv2DTranspose(1, (5, 5), strides=(2, 2), padding="same", use_bias=false, activation="tanh"),
+    )
+    // assert (model.output_shape == (None, 28, 28, 1))
+    model
+  end makeGeneratorModel
 
   def makeDiscriminatorModel(): Model[TFloat32] =
     Sequential.of(classOf[TFloat32],
@@ -48,16 +48,15 @@ object CnnGanMnist:
       Layers.conv2D(64, Seq(5, 5), Conv2D.Options(
         strides = Seq(2, 2),
         padding = Conv.Padding.Same,
-//        inputShape = Array(28, 28, 1)
       )),
-      ???, // Layers.leakyReLU(),
+      Layers.leakyReLU(),
       ???, // Layers.dropout(0.3),
 
       Layers.conv2D(128, Seq(5, 5), Conv2D.Options(
         strides = Seq(2, 2),
         padding = Conv.Padding.Same,
       )),
-      ???, // Layers.leakyReLU(),
+      Layers.leakyReLU(),
       ???, // Layers.dropout(0.3),
 
       Layers.flatten(),
